f8abf3644b1c31cd197cf713bfe7e024abbe5c44
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-13 13:10:47 +0200

    Feat:Exercise Solved

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Counter from "./App";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<Counter />, document.querySelector("#root"));
+ReactDOM.render(
+  <Counter initialCountValue={0} incrementValue={2} timerValue={1000} />,
+  document.querySelector("#root")
+);
