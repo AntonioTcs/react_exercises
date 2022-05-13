672b4180b566e9f51cd0a2dc70365b3388c9c22f
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-13 13:06:26 +0200

    Feat:Exercise Solved

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Greets extends React.Component {
   render() {
-    return <p>Welcome, {this.props.name}</p>;
+    return <p>Welcome, {this.props.name ? this.props.name : "Franco"}</p>;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import Greets from "./App";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<Greets name="John" />, document.querySelector("#root"));
+ReactDOM.render(<Greets />, document.querySelector("#root"));
