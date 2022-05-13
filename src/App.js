89be779de672f271ede3d1799dd971049e71c084
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-13 13:07:45 +0200

    Feat:Exercise Solved

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ class Greets extends React.Component {
   render() {
     return (
       <div>
-        <p>Welcome, {this.props.name ? this.props.name : "Franco"}</p>
+        <p>
+          Welcome,
+          {this.props.name ? (
+            <strong> {this.props.name}</strong>
+          ) : (
+            <strong>Franco</strong>
+          )}
+        </p>
         <p>Your age is {this.props.age}</p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Greets from "./App";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<Greets age={21} />, document.querySelector("#root"));
+ReactDOM.render(
+  <Greets name="John" age={21} />,
+  document.querySelector("#root")
+);
