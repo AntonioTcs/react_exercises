a51d9615f1bafecc38171e5906ad3cc4f9286580
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-13 11:29:04 +0200

    Feat: Exercise components-04 solved

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-import react from "react";
-import ReactDOM from "react-dom";
+import App from "./App";
+import reactDOM from "react-dom";
 
-class HelloInDiv extends react.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello!</h1>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<HelloInDiv />, document.querySelector("#root"));
+reactDOM.render(App(), document.querySelector("#root"));
