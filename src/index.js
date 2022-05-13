d7e6aabada9565e562b2a6ba42e555009a708b8d
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-13 11:35:33 +0200

    Geat Exercise components-07 solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import react from "react";
 import ReactDOM from "react-dom";
 
+class Message extends react.Component {
+  render() {
+    return <p>What a beautiful day!</p>;
+  }
+}
+
 class Hello extends react.Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
-        <p>What a beautiful day!</p>
+        <h1>Hello!</h1>
+        <Message />
       </div>
     );
   }
