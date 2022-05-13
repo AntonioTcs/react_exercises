5e91ce02230b0d58f5816fbfd518380ca420f1dd
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-13 14:04:06 +0200

    Feat: Exercise Solved

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
 import react from "react";
 
-export class ClickTracker extends react.Component {
+export class Login extends react.Component {
   state = {
-    lastClick: "None",
+    username: "",
+    password: "",
+    remember: false,
   };
 
-  clickEventHandler = (event) => {
-    this.setState((state) => {
-      return {
-        lastClick: event.target.id,
-      };
+  ChangeEventHandler = (event) => {
+    const name = event.target.name;
+    const type = event.target.type;
+    const value = event.target.value;
+    const check = event.target.checked;
+
+    this.setState({
+      [name]: type === "checkbox" ? check : value,
+    });
+  };
+
+  btnClickHandler = () => {
+    console.log(this.state);
+    this.setState({
+      username: "",
+      password: "",
+      remember: false,
     });
   };
 
   render() {
     return (
       <div>
-        <h1>Button: {this.state.lastClick}</h1>
-        <button id="one" onClick={this.clickEventHandler}>
-          Click!
-        </button>
-        <button id="two" onClick={this.clickEventHandler}>
-          Click!
-        </button>
-        <button id="three" onClick={this.clickEventHandler}>
-          Click!
-        </button>
+        <input
+          name="username"
+          value={this.state.username}
+          onChange={this.ChangeEventHandler}
+        />
+        <input
+          name="password"
+          type="password"
+          value={this.state.password}
+          onChange={this.ChangeEventHandler}
+        />
+        <input
+          name="remember"
+          type="checkbox"
+          checked={this.state.remember}
+          onChange={this.ChangeEventHandler}
+        />
+
+        <button onClick={this.btnClickHandler}>Reset</button>
       </div>
     );
   }
