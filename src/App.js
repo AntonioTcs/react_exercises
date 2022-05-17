29e0242e0341d29eb358a01362f97dfd4f0cba02
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-17 13:22:09 +0200

    Feat:Exercise Solved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import react from "react";
 import "./main.css";
+import { createContext } from "react";
 
+const languageContext = createContext("en");
 export class App extends react.Component {
-  onLogin = (state) => {
-    console.log(state);
+  state = {
+    language: "",
   };
+
+  languageChangeHandler = (event) => {
+    this.setState({
+      language: event.target.value,
+    });
+  };
+
   render() {
     return (
-      <Container title="App's Title">
-        <div>There is some content</div>
-      </Container>
+      <div>
+        <languageContext.Provider value={this.state.language}>
+          <select
+            value={this.state.language}
+            onChange={this.languageChangeHandler}
+          >
+            <option value="en">English</option>
+            <option value="it">Italiano</option>
+          </select>
+          <DisplayLanguage />
+        </languageContext.Provider>
+      </div>
     );
   }
 }
-class Container extends react.Component {
+class DisplayLanguage extends react.Component {
   render() {
     return (
       <div className="bg-red-500 border-2 border-blue-800">
-        <h1>{this.props.title}</h1>
-        {this.props.children}
+        <languageContext.Consumer>
+          {(language) => {
+            console.log(language);
+            return <div>{language === "it" ? "Ciao!" : "Hi!"}</div>;
+          }}
+        </languageContext.Consumer>
       </div>
     );
   }
