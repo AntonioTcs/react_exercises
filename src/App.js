9df051d78fcd2ca672869f06863ce0cb0100cb5d
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-17 12:15:34 +0200

    Feat:Exercise Solved

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends react.Component {
   };
   render() {
     return (
-      <Container>
+      <Container title="App's Title">
         <div>There is some content</div>
       </Container>
     );
@@ -17,6 +17,7 @@ class Container extends react.Component {
   render() {
     return (
       <div className="bg-red-500 border-2 border-blue-800">
+        <h1>{this.props.title}</h1>
         {this.props.children}
       </div>
     );
