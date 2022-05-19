6a8925a41fb8af8522c38499180b85c0e60d6f00
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-19 11:07:49 +0200

    Feat:Exercise Solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-export function Sum({ numbers = [0] }) {
+import { useState } from "react";
+
+export function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function clickEventHandler() {
+    setCounter((counter) => counter + 1);
+  }
+
   return (
     <div>
-      <h1>
-        Result: {numbers.reduce((prev, currentValue) => prev + currentValue)}
-      </h1>
+      <h1>Count: {counter}</h1>
+      <button onClick={clickEventHandler}>Click!</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { Sum } from "./App";
+import { Counter } from "./App";
 import ReactDOM from "react-dom";
 
 let rootElement = document.querySelector("#root");
-const array = [1, 5, 6, 8, 7, 4, 5, 3];
-ReactDOM.render(<Sum /* numbers={array} */ />, rootElement);
+
+ReactDOM.render(<Counter />, rootElement);
