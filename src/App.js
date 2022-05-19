9f592fb6ccfe1c3305453d2cd54bfcc671a43f06
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-19 17:22:49 +0200

    Exercise not finished

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import { useEffect, useState } from "react";
-
-function changeFunction(number) {
-  console.log(`Counter has changed. Current value is ${number}`);
-}
+import React, { useEffect, useState } from "react";
 
 export function App() {
+  const [view, setView] = useState(true);
+
+  function buttonClickHandle() {
+    setView((view) => !view);
+  }
+
   return (
     <div>
-      <Counter onCounterChange={changeFunction} />
+      <button onClick={buttonClickHandle}>Unmount Counter</button>
+      {view && <Counter />}
     </div>
   );
 }
 
-function Counter({ onCounterChange }) {
-  const [counter, setCounter] = useState(0);
-
-  function clickEventHandler() {
-    setCounter((counter) => counter + 1);
-  }
+function Counter() {
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    onCounterChange(counter);
-  }, [counter, onCounterChange]);
+    console.log("Component mounted and interval is going to start");
+    const interval = setInterval(() => {
+      setCount((count) => {
+        count++;
+      });
+    }, 1000);
+
+    return () => {
+      console.log("Clearing interval");
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("Counter Changed");
+  }, [count]);
 
   return (
     <div>
-      <h1>Count: {counter}</h1>
-      <button onClick={clickEventHandler}>Click!</button>
+      <h1>Count: {count}</h1>
     </div>
   );
 }
