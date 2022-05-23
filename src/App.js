0058bd8ee7e7544b55bb0038144cbfd7118a37f5
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-23 20:36:05 +0200

    Feat:Exercise Solved

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import { useForm } from "./useForm";
+import { useMemo } from "react";
 
+const list = [
+  { name: "John", id: 1, age: 20 },
+  { name: "Foo", id: 2, age: 17 },
+  { name: "Jonathan", id: 3, age: 16 },
+  { name: "Antony", id: 4, age: 19 },
+  { name: "Marcus", id: 5, age: 20 },
+];
 export function App() {
-  return <Form />;
+  return <FilteredList list={list} />;
 }
 
-function Form() {
-  const { user, handleInputChange } = useForm;
-
-  function handleChange(event) {
-    handleInputChange(event);
-  }
-  return (
-    <div>
-      <input name="username" onChange={handleChange} />
-      <input type="password" name="password" onChange={handleChange} />
-    </div>
+function FilteredList({ objList }) {
+  const listToRender = useMemo(
+    () =>
+      list.map((user, index) =>
+        user.age > 18 ? (
+          <div key={index}>
+            <h1>Name: {user.name}</h1>
+            <h2>Id: {user.id}</h2>
+            <h2>Age: {user.age}</h2>
+          </div>
+        ) : null
+      ),
+    []
   );
+  return <div>{listToRender}</div>;
 }
