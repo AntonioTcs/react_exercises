b353b93700115dfd0c90ea4902302c22532d40ee
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-24 11:01:25 +0200

    Feat:In progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Routes, Route, useParams } from "react-router-dom";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome name="John" />} />
+      <Route path="users:username" element={<ShowGitHubUser />} />
     </Routes>
   );
 }
@@ -12,6 +14,32 @@ function Welcome({ name = "World" }) {
   return (
     <div>
       <h1>Welcome {name}</h1>
+    </div>
+  );
+}
+
+function ShowGitHubUser() {
+  const { username } = useParams();
+  return <GitHubUser username={username} />;
+}
+
+function GitHubUser({ username }) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${username}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+      });
+  }, [username]);
+
+  return (
+    <div>
+      <h1>{`User: ${data.name} `}</h1>
+      <h3>{`Bio: ${data.bio} `}</h3>
     </div>
   );
 }
