e2046bf1417e4613ae8fdf870c646e31b5e7d538
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-24 09:20:22 +0200

    Feat: Exercise Solved

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import { useForm } from "./useForm";
+import { useContext, useState } from "react";
+import { languageContext } from "./LanguageContext";
 
 export function App() {
-  return <Form />;
-}
+  const [language, setLanguage] = useState("en");
 
-function Form() {
-  const { user, handleInputChange } = useForm;
-
-  function handleChange(event) {
-    handleInputChange(event);
+  function handleLanguageChange(event) {
+    setLanguage(event.target.value);
   }
   return (
-    <div>
-      <input name="username" onChange={handleChange} />
-      <input type="password" name="password" onChange={handleChange} />
+    <languageContext.Provider value={language}>
+      <select value={language} onChange={handleLanguageChange}>
+        <option value="en">English</option>
+        <option value="it">Italiano</option>
+      </select>
+      <DisplayLanguage />
+    </languageContext.Provider>
+  );
+}
+
+function DisplayLanguage() {
+  const language = useContext(languageContext);
+
+  return (
+    <div className="bg-red-500 border-2 border-blue-800">
+      <h1>
+        {language === "it" ? "Lingua selezionata: " : "Selected Language: "}
+        {language}
+      </h1>
     </div>
   );
 }
