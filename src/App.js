f0231e2448715fef58fc2e85767866457a111e51
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-24 10:45:15 +0200

    Feat:Exercise Solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome name="John" />} />
+      <Route path="counter" element={<Counter />} />
     </Routes>
   );
 }
@@ -12,6 +14,21 @@ function Welcome({ name = "World" }) {
   return (
     <div>
       <h1>Welcome {name}</h1>
+    </div>
+  );
+}
+
+export function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function clickEventHandler() {
+    setCounter((counter) => counter + 1);
+  }
+
+  return (
+    <div>
+      <h1>Count: {counter}</h1>
+      <button onClick={clickEventHandler}>Click!</button>
     </div>
   );
 }
