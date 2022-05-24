1f03fc33192cd207616aa7317ea45c5e1a4883dc
AntonioTcs <dv.antonio.scetta@gmail.com>
2022-05-24 12:50:15 +0200

    Feat:Exercise Solved

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ export function App() {
 }
 
 function GithubUser({ username }) {
-  const { result } = useGithubUser(username);
+  const { result, fetcher, isLoading, error } = useGithubUser(username);
 
   return (
     <div>
-      {!result && <h1>Loading...</h1>}
-      {result && (
+      {isLoading && <h1>Loading...</h1>}
+      {result && !error && (
         <div>
           <h1>User: {result.name}</h1> <h2>Bio: {result.bio}</h2>
         </div>
       )}
+      {error && <h1>There was an error during the request</h1>}
     </div>
   );
 }
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -2,11 +2,29 @@ import { useEffect, useState } from "react";
 
 export function useGithubUser(username) {
   const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  function fetcher() {
+    fetch(`https://api.github.com/users/${username}`)
+      .then((response) => {
+        if (response.status !== 200) {
+          setError(true);
+          return null;
+        }
+        return response.json();
+      })
+      .then((json) => {
+        setData(json);
+        setLoading(false);
+      });
+  }
 
   useEffect(() => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => response.json())
-      .then((json) => setData(json));
+    setError(false);
+    setLoading(true);
+    fetcher();
   }, []);
-  return { result: data };
+
+  return { result: data, fetcher, isLoading, error };
 }
